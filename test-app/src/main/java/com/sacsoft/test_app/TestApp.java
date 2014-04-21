b271db5e719cc8481eef02317270fdbdcf283b0b
sachinjain0101 <sachinjain.0101@gmail.com>
2014-04-21 16:04:43 +0530

    New Test App

---
+++ test-app/src/main/java/com/sacsoft/test_app/TestApp.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2009 IT Mill Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.sacsoft.test_app;
+
+import com.vaadin.Application;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.Tree;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+/**
+ * The Application's "main" class
+ */
+@SuppressWarnings("serial")
+public class TestApp extends Application {
+	private Window window;
+	private VerticalLayout vLayout;
+	private HorizontalLayout hLayout;
+
+	private TabSheet tabSheet;
+
+	private Panel panel1;
+	private Panel panel2;
+
+	private Button clearMe;
+
+	@Override
+	public void init() {
+		window = new Window("TestApp - SaCsOfT");
+		setMainWindow(window);
+
+		window.setTheme(getTheme());
+
+		vLayout = new VerticalLayout();
+		window.setContent(vLayout);
+		window.getContent().setSizeFull();
+
+		vLayout.addComponent(new Label("TestApp - SaCsOfT"));
+
+		tabSheet = new TabSheet();
+		vLayout.addComponent(tabSheet);
+
+		hLayout = new HorizontalLayout();
+		hLayout.setSizeFull();
+		HorizontalLayout dummy = new HorizontalLayout();
+		tabSheet.addTab(hLayout, "HL1");
+		tabSheet.addTab(dummy, "HL2");
+		// vLayout.addComponent(hLayout);
+		// hLayout.setMargin(true);
+
+		panel1 = new Panel("Button Panel");
+		panel1.setSizeFull();
+		panel2 = new Panel("Message Panel");
+		panel2.setSizeFull();
+
+		Button button = new Button("Click Me");
+		panel1.addComponent(button);
+		hLayout.addComponent(panel1);
+
+		clearMe = new Button("Clear Me");
+		panel2.addComponent(clearMe);
+
+		hLayout.addComponent(panel2);
+
+		Tree tree = new Tree("Test Tree");
+		tree.setSizeFull();
+		String name = "Sachin Jain";
+		tree.addItem(name);
+		hLayout.addComponent(tree);
+
+		button.addListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				panel2.addComponent(new Label("Thank you for clicking"));
+			}
+		});
+
+		clearMe.addListener(new Button.ClickListener() {
+
+			public void buttonClick(ClickEvent event) {
+				panel2.removeAllComponents();
+				panel2.addComponent(clearMe);
+			}
+		});
+
+	}
+
+}
